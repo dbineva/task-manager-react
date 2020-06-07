b901edde4a2e96ec8c1af6774da5c40d9fee6122
dbineva <dianabinevabineva@gmail.com>
2020-06-07 12:35:34 +0300

    Fixed

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const styles = {
     backgroundColor: "rgb(52,58,64)",
-    height: "150px",
+    height: "120px",
     width: "100%",
     position: "fixed",
     bottom: 0
